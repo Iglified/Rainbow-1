a9081d329230ecc3f3a90540637a345a2b36e689
Colin Weir <colin@radioboy.us>
2018-06-11 21:32:52 -0400

    Add files via upload

---
+++ Rainbow/Rainbow/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Rainbow
+//
+//  Created by Colin Weir on 6/11/18.
+//  Copyright © 2018 Tukey Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func turnRed(_ sender: Any) {
+        view.backgroundColor = .red
+    }
+    
+    @IBAction func turnOrange(_ sender: Any) {
+        view.backgroundColor = .orange
+    }
+    
+    @IBAction func turnYellow(_ sender: Any) {
+        view.backgroundColor = .yellow
+    }
+    
+    @IBAction func turnGreen(_ sender: Any) {
+        view.backgroundColor = .green
+    }
+    
+    @IBAction func turnBlue(_ sender: Any) {
+        view.backgroundColor = .blue
+    }
+    
+    @IBAction func turnViolet(_ sender: Any) {
+        view.backgroundColor = .purple
+    }
+
+}
+
